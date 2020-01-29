b71a42f138ea8fd93444201871f58a73b9f7b505
JeongByeongHun <jbh1024@gmail.com>
2020-01-29 12:48:24 +0900

    first commit

---
+++ src/main/java/org/iptime/hoonyhoony/springboot_tacademy_study/SpringBootTacademyStudyApplication.java
@@ -0,0 +1,13 @@
+package org.iptime.hoonyhoony.springboot_tacademy_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTacademyStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTacademyStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/iptime/hoonyhoony/springboot_tacademy_study/SpringBootTacademyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iptime.hoonyhoony.springboot_tacademy_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTacademyStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
